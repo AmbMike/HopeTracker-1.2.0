715fd965eda01aa9b98e63ddb5c1ac7c36cb04ff
ambMike <mgiammattei@ambrosiatc.com>
2018-04-19 11:40:54 -0400

    Fixed the "liked post" notification link and link value.

--- classes/class.CommunityNotification.php
+++ classes/class.CommunityNotification.php
@@ -429,8 +429,8 @@ class CommunityNotification
              * adding the link properties.
              * @var $notificationLink
              */
-            $href = '/families-of-drug-addicts/journal-' . $function['liked_journal'][0]['id'] . '/' . $this->General->url_safe_string($function['liked_journal'][0]['title']) . '/';
-            $linkValue = $function['liked_journal'][0]['title'];
+            $href = '/families-of-drug-addicts/journal-' . $function['liked_journal'][0]['id'] . '/' . $this->General->url_safe_string($this->General->trim_text($function['liked_journal'][0]['content'],'40',true,true)) . '/';
+	        $linkValue = ($function['liked_journal'][0]['title']) ? $function['liked_journal'][0]['title'] : $this->General->trim_text($function['liked_journal'][0]['content'],'40',true,true);
             $options = array(
                 'class' => 'notification-link'
             );
@@ -820,7 +820,7 @@ class CommunityNotification
              * Create the outbound link.
              */
             $linkContent = (!empty($function['title'])) ? $function['title'] : substr($function['content'],0,50);
-	        $href = '/' . RELATIVE_PATH  . 'families-of-drug-addicts/journal-' .  $function['id'] .'/' .$this->General->url_safe_string($linkContent);
+	        $href = '/families-of-drug-addicts/journal-' .  $function['id'] .'/' .$this->General->url_safe_string($linkContent);
             $linkValue = $linkContent;
             $options = array(
                 'class' => 'notification-link'
